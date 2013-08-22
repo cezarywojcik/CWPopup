40fb9b9ce8902c1d947d69806709b69eb864ec0c
Cezary Wojcik <cezarywojcik92@gmail.com>
2013-08-21 20:36:26 -0700

    Initial Commit

---
+++ CWPopupDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CWPopupDemo
+//
+//  Created by Cezary Wojcik on 8/21/13.
+//  Copyright (c) 2013 Cezary Wojcik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CWAppDelegate class]));
+    }
+}
