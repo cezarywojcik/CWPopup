11fa20d83dec1f65785fc620ec7e432c6b2fbb67
zhengxin <xin.zheng@qaik.com>
2014-05-15 10:35:46 +0800

    add blur navigationBar add handle tap blurView

--- CWPopup/UIViewController+CWPopup.h
+++ CWPopup/UIViewController+CWPopup.h
@@ -12,11 +12,15 @@
 
 @property (nonatomic, readwrite) UIViewController *popupViewController;
 @property (nonatomic, readwrite) BOOL useBlurForPopup;
+@property (nonatomic, strong) UIColor *tintColorForBlurredBackground;
 @property (nonatomic, readwrite) CGPoint popupViewOffset;
 
 - (void)presentPopupViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion;
 - (void)dismissPopupViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion;
+- (void)tappedOutsidePresentedPopupViewController:(UITapGestureRecognizer *)gestureRecognizer;
 - (void)setUseBlurForPopup:(BOOL)useBlurForPopup;
 - (BOOL)useBlurForPopup;
+- (void)setTintColorForBlurredBackground:(UIColor *)tintColor;
+- (UIColor *)tintColorForBlurredBackground;
 
 @end
